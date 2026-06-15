0cbee2b7596cd13c57dc32528dd45daac0195235
George <george.bobovsky@gmail.com>
2026-06-15 15:57:35 +0500

    hw-3 (implemented review suggestions)

--- src/app/(app)/racket/[racketId]/loading.tsx
+++ src/app/(app)/racket/[racketId]/loading.tsx
@@ -1,8 +1,3 @@
 import Spinner from "@/components/spinner/Spinner";
-import { FC } from "react";
 
-const RacketLoading: FC = function () {
-  return <Spinner />;
-};
-
-export default RacketLoading;
+export default Spinner;
